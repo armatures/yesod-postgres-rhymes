2efd47dd37d25bae59f3a0abd5cb358a39d7dc39
Charlie Bevis <charliebevis@gmail.com>
2017-05-19 12:48:27 -0700

    seed phonemes

--- seed/Seed.hs
+++ seed/Seed.hs
@@ -21,42 +21,43 @@ main = do
     semivowelId <- insert $ PhonemeType "semivowel"
     stopId <- insert $ PhonemeType "stop"
     vowelId <- insert $ PhonemeType "vowel"
-    insert_ $ Phoneme "AA" vowelId
-    insert_ $ Phoneme "AE" vowelId
-    insert_ $ Phoneme "AH" vowelId
-    insert_ $ Phoneme "AO" vowelId
-    insert_ $ Phoneme "AW" vowelId
-    insert_ $ Phoneme "AY" vowelId
-    insert_ $ Phoneme "B" stopId
-    insert_ $ Phoneme "CH" affricateId
-    insert_ $ Phoneme "D" stopId
-    insert_ $ Phoneme "DH" fricativeId
-    insert_ $ Phoneme "EH" vowelId
-    insert_ $ Phoneme "ER" vowelId
-    insert_ $ Phoneme "EY" vowelId
-    insert_ $ Phoneme "F" fricativeId
-    insert_ $ Phoneme "G" stopId
-    insert_ $ Phoneme "HH" aspirateId
-    insert_ $ Phoneme "IH" vowelId
-    insert_ $ Phoneme "IY" vowelId
-    insert_ $ Phoneme "JH" affricateId
-    insert_ $ Phoneme "K" stopId
-    insert_ $ Phoneme "L" liquidId
-    insert_ $ Phoneme "M" nasalId
-    insert_ $ Phoneme "N" nasalId
-    insert_ $ Phoneme "NG" nasalId
-    insert_ $ Phoneme "OW" vowelId
-    insert_ $ Phoneme "OY" vowelId
-    insert_ $ Phoneme "P" stopId
-    insert_ $ Phoneme "R" liquidId
-    insert_ $ Phoneme "S" fricativeId
-    insert_ $ Phoneme "SH" fricativeId
-    insert_ $ Phoneme "T" stopId
-    insert_ $ Phoneme "TH" fricativeId
-    insert_ $ Phoneme "UH" vowelId
-    insert_ $ Phoneme "UW" vowelId
-    insert_ $ Phoneme "V" fricativeId
-    insert_ $ Phoneme "W" semivowelId
-    insert_ $ Phoneme "Y" semivowelId
-    insert_ $ Phoneme "Z" fricativeId
+    aa <- insert $ Phoneme "AA" vowelId
+    ae <- insert $ Phoneme "AE" vowelId
+    ah <- insert $ Phoneme "AH" vowelId
+    ao <- insert $ Phoneme "AO" vowelId
+    aw <- insert $ Phoneme "AW" vowelId
+    ay <- insert $ Phoneme "AY" vowelId
+    b <- insert $ Phoneme "B" stopId
+    ch <- insert $ Phoneme "CH" affricateId
+    d <- insert $ Phoneme "D" stopId
+    dh <- insert $ Phoneme "DH" fricativeId
+    eh <- insert $ Phoneme "EH" vowelId
+    er <- insert $ Phoneme "ER" vowelId
+    ey <- insert $ Phoneme "EY" vowelId
+    f <- insert $ Phoneme "F" fricativeId
+    g <- insert $ Phoneme "G" stopId
+    hh <- insert $ Phoneme "HH" aspirateId
+    ih <- insert $ Phoneme "IH" vowelId
+    iy <- insert $ Phoneme "IY" vowelId
+    jh <- insert $ Phoneme "JH" affricateId
+    k <- insert $ Phoneme "K" stopId
+    l <- insert $ Phoneme "L" liquidId
+    m <- insert $ Phoneme "M" nasalId
+    n <- insert $ Phoneme "N" nasalId
+    ng <- insert $ Phoneme "NG" nasalId
+    ow <- insert $ Phoneme "OW" vowelId
+    oy <- insert $ Phoneme "OY" vowelId
+    p <- insert $ Phoneme "P" stopId
+    r <- insert $ Phoneme "R" liquidId
+    s <- insert $ Phoneme "S" fricativeId
+    sh <- insert $ Phoneme "SH" fricativeId
+    t <- insert $ Phoneme "T" stopId
+    th <- insert $ Phoneme "TH" fricativeId
+    uh <- insert $ Phoneme "UH" vowelId
+    uw <- insert $ Phoneme "UW" vowelId
+    v <- insert $ Phoneme "V" fricativeId
+    w <- insert $ Phoneme "W" semivowelId
+    y <- insert $ Phoneme "Y" semivowelId
+    z <- insert $ Phoneme "Z" fricativeId
     insert_ $ Phoneme "ZH" fricativeId
+    -- insert_ $ Pronounciation "BOW" [b, ow]
