f8ff1472ced084b618f23c5c0a024a93f54de72f
Charlie Bevis <charliebevis@gmail.com>
2017-08-01 14:19:20 -0500

    pronunciation JSON endpoint

--- rhymebook/Handler/Pronounce.hs
+++ rhymebook/Handler/Pronounce.hs
@@ -1,9 +1,9 @@
 module Handler.Pronounce where
 
 import Import
-getPronounceR :: Text -> Handler Html
-getPronounceR spelling =
-    defaultLayout $ do
-        setTitle $ toHtml ("pronounce " ++ spelling)
-        $(widgetFile "pronounce")
+getPronounceR :: Text -> Handler Value
+getPronounceR _spelling = do
+    pros <- runDB $ selectList [PronunciationSpelling ==. _spelling] [LimitTo 1]
+
+    return $ object ["pronunciations" .= pros]
 
--- rhymebook/Rhymebook/Model.hs
+++ rhymebook/Rhymebook/Model.hs
@@ -4,10 +4,17 @@ module Rhymebook.Model where --(Emphasis, Phoneme, Pronunciation)  where
 
 import ClassyPrelude.Yesod
 import Database.Persist.Quasi
+import Data.Text as T
 
 data Emphasis = Emp2 | Emp1 | Emp0 | EmpNone
     deriving (Eq, Read, Show)
 derivePersistField "Emphasis"
+
+instance ToJSON Emphasis where
+    toJSON a = String $ T.pack (show a)
+
+instance FromJSON Emphasis where
+    parseJSON = undefined
 
 data Phoneme = AA Emphasis | AE Emphasis | AH Emphasis | AO Emphasis |
     AW Emphasis | AY Emphasis | B | CH | D | DH | EH Emphasis | ER Emphasis |
@@ -17,9 +24,16 @@ data Phoneme = AA Emphasis | AE Emphasis | AH Emphasis | AO Emphasis |
     deriving (Show, Read, Eq)
 derivePersistField "Phoneme"
 
+instance ToJSON Phoneme where
+    toJSON a = String $ T.pack (show a)
+
+instance FromJSON Phoneme where
+    parseJSON = undefined
+
 -- You can define all of your database entities in the entities file.
 -- You can find more information on persistent and how to declare entities
 -- at:
 -- http://www.yesodweb.com/book/persistent/
 share [mkPersist sqlSettings, mkMigrate "migrateAll"]
     $(persistFileWith lowerCaseSettings "config/models")
+
